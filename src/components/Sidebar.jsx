79542389472db2340605d326e8ad95390188bb1f
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-10-04 11:57:05 +0200

    Add files via upload

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+      <footer className={styles.footer}></footer>
+      <p className={styles.copyright}>
+        &copay; Copyright {new Date().getFullYear()} by WorldWise Inc.
+      </p>
+    </div>
+  );
+}
+
+export default Sidebar;
